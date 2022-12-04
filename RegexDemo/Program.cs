12b4727b0fa41415dd72a23d33da01c69549ceb4
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-12-04 18:22:58 +0530

    implemented the use case 1 to enter a valid First Name - First name starts with Cap and has minimum 3 characters

---
+++ RegexDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns patterns = new Patterns();
+            Console.WriteLine("enter the pincode");
+            string pincode = Console.ReadLine();
+            bool isMatch = patterns.ValidatePinCode(pincode);
+            if (isMatch)
+                Console.WriteLine("pincode is validated");
+            else
+                Console.WriteLine("pinncode is not validated");
+
+            Console.WriteLine("enter the first name");
+            string firstName = Console.ReadLine();
+            bool isMatchFname = patterns.ValidateFirstName(firstName);
+            if (isMatchFname)
+                Console.WriteLine("first name is validated");
+            else
+                Console.WriteLine("first name is not validated");
+            Console.ReadLine(); 
+        }
+    }
+}
